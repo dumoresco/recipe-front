166fff5d712821c6d49b1e290704fa9ba973f8bd
Eduardo Moresco <eduardomorescoiost@gmail.com>
2022-09-19 13:44:34 -0300

    update create recipe page

---
+++ src/pages/CreateRecipe/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const CreateRecipe: React.FC = () => {
+  return <Container>new recipe</Container>;
+};
+
+export default CreateRecipe;
